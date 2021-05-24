4d4523a61d1e0a4b97e6ee6118ba9bdf6f0c8870
PingTouG <2798874773@qq.com>
2021-05-24 17:42:25 +0800

    chore: 添加路由跳转进度条

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+	import 'nprogress/nprogress.css'
 	import BackTop from '$lib/BackTop/index.svelte';
+	import NProgress from 'nprogress'
+
+	NProgress.configure({ showSpinner: false })
+	const onNavigationStart = () => {
+		console.log('start')
+		NProgress.start()
+	}
+	const onNavigationEnd = () => {
+		console.log('end')
+		NProgress.done()
+	}
 </script>
 
+<svelte:window on:sveltekit:navigation-start="{onNavigationStart}" on:sveltekit:navigation-end="{onNavigationEnd}"  />
 <section>
 	<header>
 		<a href="/">
